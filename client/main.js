df2410da2a81a44a8e644659cbc9a67f2b87e050
chequekoluoch <chequekoluoch1994@gmail.com>
2020-07-31 13:28:34 +0300

    first commit for the dbconect

---
+++ client/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+  loadHTMLTable([]);  
+})
+ function loadHTMLTable(data){
+     const table=document.querySelector('table tbody');
+     //let tableHtml="";
+     if(data.length === 0){
+         table.innerHTML="<tr><td class='no-data' colspan='5'>No data</td></tr>"
+     }
+ } 
